a90572eb82e80a8870cc3aa0307cf7b7a1bd8127
igor <igor.ferro.parreira@gmail.com>
2025-02-01 07:55:34 -0300

    project structure

---
+++ src/main/java/com/example/ApiProtocols/ApiProtocolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiProtocols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProtocolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProtocolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiProtocols/ApiProtocolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiProtocols;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProtocolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
