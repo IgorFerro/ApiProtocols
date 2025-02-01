3b30db43b6057aec6ab127bf3ac91855e9abe76d
igor <igor.ferro.parreira@gmail.com>
2025-02-01 09:00:41 -0300

    change for the correctly class

--- src/main/java/com/example/ApiProtocols/spacex/SpaceXApplication.java
+++ src/main/java/com/example/ApiProtocols/spacex/SpaceXApplication.java
@@ -1,13 +1,5 @@
 package com.example.ApiProtocols.spacex;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix= "spacex")
-@Data
 public class SpaceXApplication {
 
-    private String apiUrl = "https://api.spacex.land/graphql/";
 }
--- src/main/java/com/example/ApiProtocols/spacex/config/SpaceXConfig.java
+++ src/main/java/com/example/ApiProtocols/spacex/config/SpaceXConfig.java
@@ -1,4 +1,12 @@
 package com.example.ApiProtocols.spacex.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix= "spacex")
+@Data
 public class SpaceXConfig {
+    private String apiUrl = "https://api.spacex.land/graphql/";
 }
